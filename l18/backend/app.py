c0896ee30ed6bb774e375589c6bbe2ef3748f206
Ilya Beltsiukou <968597@gmail.com>
2021-04-15 21:19:57 +0300

    lecture 18

--- l18/backend/app.py
+++ l18/backend/app.py
@@ -1,18 +1,24 @@
-from datetime import date
-
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///flask.sqlite'
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html', title='Main page', age=16)
+    return render_template('index.html', title='Main page', age=16, users=[{'name': 'Ilya'}, {'name': 'Vania'}])
 
-class User:
-    firstname = ''
-    lastname = ''
-    age = 0
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column('id', db.Integer, primary_key=True)
+    firstname = db.Column(db.String(32))
+    lastname = db.Column(db.String(32))
+    age = db.Column(db.Integer)
+    email = db.Column(db.String(255))
 
     def __init__(self, form):
         self.firstname = form['firstname']
@@ -28,5 +34,6 @@ def form():
 
 
 if __name__ == '__main__':
-    # print(app.url_map)
+    db.init_app(app)
+    db.create_all()
     app.run(port=8000)
